89592d5c3a09c5b9c6c23a194413b9dd932af4d9
mp <michael.pinaud@gmail.com>
2018-02-24 17:29:33 -0800

    configure index.jsx, App.jsx and run succesful build and test

--- webpack.config.js
+++ webpack.config.js
@@ -76,7 +76,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'POP!',
+      title: 'POP',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
