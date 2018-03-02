d7a86a11f37d100b3661a16f7a26cb763ea3c9e9
mp <michael.pinaud@gmail.com>
2018-03-02 10:17:47 -0800

    create PopObjectInfo component including placeholder title and unordered list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import HomeLogo from './HomeLogo';
 import Hero from './Hero';
+import PopObjectInfo from './PopObjectInfo';
+import HistoryButton from './HistoryButton';
 
 function Header() {
   return (
     <div>
       <HomeLogo />
       <Hero />
+      <PopObjectInfo />
+      <HistoryButton />
     </div>
   );
 }
---
+++ src/components/PopObjectInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function PopObjectInfo() {
+  return (
+    <div>
+      <h1>#IHeartAwards</h1>
+      <ul>
+        <li>D.O.B.</li>
+        <li>Birth Size</li>
+        <li>Start Users</li>
+        <li>Current Users</li>
+        <li>Age</li>
+      </ul>
+    </div>
+  );
+}
+
+export default PopObjectInfo;
