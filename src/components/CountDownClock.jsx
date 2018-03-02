1881b7480228a98badab99d3c3c394e1f0e300ad
mp <michael.pinaud@gmail.com>
2018-03-02 15:06:24 -0800

    add Custom Typeface to README for research

--- src/components/CountDownClock.jsx
+++ src/components/CountDownClock.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import glamorous from 'glamorous';
+
+const Heading = glamorous.h1({
+  fontSize: '2.4em',
+  marginTop: 10,
+  color: '#CC3A4B',
+});
+
 
 function CountDownClock() {
   return (
     <div>
-      5:00
+      <Heading>5:00</Heading>
     </div>
   );
 }
