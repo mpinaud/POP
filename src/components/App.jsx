669923d48703d8618a492d01c0c113574a3d7ac5
mp <michael.pinaud@gmail.com>
2018-03-02 09:13:51 -0800

    add Notes section to README adding OAuth 2 specification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Twit from 'twit';
 
 function App() {
+  const T = new Twit({
+    consumer_key:         'VDtPhz4zEg9kRO8mSWHRL47Ct',
+    consumer_secret:      '01Fij55PF075HKKZ8Nq9tbFBTWkeW5b2J6nbETTeSfjhTnju4W',
+    access_token:         '968260387345907712-LUwCOpqKgcCybwE4OIk4yLejRgkXbKZ',
+    access_token_secret:  'L4uSejBr5iO5MGz53HbWC2Cf5MixM6YEXGyWVGfGq7hLO',
+  });
+
+  T.get('search/tweets', { q: 'banana since:2017-07-11', count: 100 }, function(err, data, response) {
+    console.log(data);
+  });
+  
   return (
     <div>
       POP!
     </div>
   );
 }
+
+
+
 
 export default App;
