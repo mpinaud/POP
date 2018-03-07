6979ccfbad8d25ddf7128be61b949bc9f01116f5
Student <example@example.com>
2018-03-07 14:48:28 -0800

    create express server

---
+++ src/components/twitter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Twitter extends React.Component{
+  render() {
+    return(
+      <p>This is working</p>
+    );
+  }
+}
+
+Twits.propTypes = {
+};
+
+export default Twitter;
