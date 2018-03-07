d76f60e08f310ca48437abe1eddce13638b250b7
mikey pinaud <michael.pinaud@gmail.com>
2018-03-07 15:23:50 -0800

    create Twitter jsx

--- src/components/twitter.jsx
+++ src/components/twitter.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class Twitter extends React.Component{
+  authorize() {
+    axios.post('http://localhost:3000/authorize', {'Content-Type': 'application/X-www-form-urlencoded'})
+    .then(res => {
+      console.log(res);
+    })
+  }
+
+  componentDidMount() {
+  this.authorize();
+  }
+
   render() {
+    console.log('Twitter component works');
     return(
       <p>This is working</p>
     );
   }
 }
 
-Twits.propTypes = {
+Twitter.propTypes = {
 };
 
 export default Twitter;
+
+
+// export class TwitterApiService {
+//   tweetsdata;
+//
+//   constructor(private http: Http) { }
+//
+//   postToObtainABearerToken() {
+//     var headers = new Headers();
+//     headers.append('Content-Type', 'application/X-www-form-urlencoded');
+//
+//     return this.http.post(
+//       'http://localhost:3000/authorize', { headers: headers }
+//     );
+//   }
+//
+//   getData() {
+//     var headers = new Headers();
+//     headers.append('Content-Type', 'application/X-www-form-urlencoded');
+//
+//     return this.http.post('http://localhost:3000/top', {headers: headers});
+//   }
+// }
